7d242f4034625849c68ad67969fe7129b8d0095f
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2020-11-23 16:57:55 -0300

    update js 23112020

--- JS/main.js
+++ JS/main.js
@@ -3,62 +3,81 @@
 let dados = null;
 const token = 1687915884717809;
 
+const randomId = () => {
+    const number = Math.floor(Math.random() * 732);
+    return number;
+};
+
+
 const insertDataToCards = (data) => {
     dados = data;
     console.log(dados);
 };
 
 const fetchInformation  = (url , type) => {
+
+    const getCharacter = (alignment) => {
+        const number = randomId();
+        const url = `https://www.superheroapi.com/api.php/${token}/${number}`;
+        console.log(url);
+        fetch(url).then(response => response.json()).then(data => {
+            if (data.biography.alignment==alignment) {
+                insertDataToCards(data);
+            }else {
+                return getCharacter(alignment);
+            }
+        });
+
+    };
+
     switch (type) {
-        case ("none"):
-            fetch(url).then(response => response.json()).then(data => insertDataToCards(data));
-        break;
+
+        //case:
 
         case ("hero"):
-            let dadosRecebidos = null;
-            fetch(url).then(response => response.json()).then(data => dadosRecebidos = data);
-            dadosRecebidos.forEach(element => {
-                
-            });
+            getCharacter("good");
+        break;
+
+        case ("villain"):
+            getCharacter("bad");
+        break;
+
+        case ("neutral"):
+            getCharacter("neutral");
+        break;
+
+        default:
+            fetch(url).then(response => response.json()).then(data => insertDataToCards(data));
         break;
     }
 };
 
 const getByName = (nome) => {
-
     const url = `https://www.superheroapi.com/api.php/${token}/search/${nome}`;
-    fetchInformation(url , "none");
+    fetchInformation(url);
 };
 
 const getById = (id) => {
 
     const url = `https://www.superheroapi.com/api.php/${token}/${id}`;
-    fetchInformation(url , "none");
+    fetchInformation(url);
 };
 
 const randomCard = () => {
-    const number = Math.floor(Math.random() * 732);
-
+    const number = randomId;
     const url = `https://www.superheroapi.com/api.php/${token}/${number}`;
-    fetchInformation(url, "none");
+    fetchInformation(url, "random");
 }
 
 
-const randomSixCard = () => {
-    for (let i = 1 ; i <=6 ; i++) {
-    const number = Math.floor(Math.random() * 732);
+const getCharacterByAlignment = (alignment) => {
+    fetchInformation( "" , alignment);
+}
 
-    const url = `https://www.superheroapi.com/api.php/${token}/${number}`;
-    fetchInformation(url, "none");
+const all = () => {
+    for (let i = 1 ; i < 722; i++) {
+    const url = `https://www.superheroapi.com/api.php/${token}/${i}`;
+    fetchInformation(url);
     }
-}
-
-const randomHero = () => {
-    for (let i = 1 ; i <=6 ; i++) {
-    const number = Math.floor(Math.random() * 732);
-
-    const url = `https://www.superheroapi.com/api.php/${token}/${number}`;
-    fetchInformation(url , "hero");
-    }
-}
+};
 
